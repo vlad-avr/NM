5d54302318245fb2ab5c8317fea04ec9b0729052
vlad-avr <99638385+vlad-avr@users.noreply.github.com>
2023-11-06 12:02:59 +0200

    added current result output for each iteration

--- Lab3/lab3.py
+++ Lab3/lab3.py
@@ -3,15 +3,23 @@ import math as m
 
 def modified_newton_method(f, jacobian, x0, tol=1e-6, max_iter=100):
     x = x0
-
+    print("x(0) : ", x0)
     for i in range(max_iter):
         f_x = f(x)
         jacobian_x = jacobian(x)
-        delta_x = -np.dot(np.linalg.inv(jacobian_x), f_x)
+        print("\nJacobian : \n", jacobian_x)
+        if abs(np.linalg.det(jacobian_x)) < tol:
+            print("\n Convergence conditions are not met : det(A) == 1")
+        jacobian_inv = np.linalg.inv(jacobian_x)
+        print("\nJacobian inverse : \n", jacobian_inv)
+        print("F(x(", i,")) : ", f_x)
+        delta_x = -np.dot(jacobian_inv, f_x)
+        print("\n z(", i+1, ") : ", delta_x)
         x = x + delta_x
+        print("x(", i+1,") : ", x)
         if np.linalg.norm(delta_x) < tol:
             return x
-    print("NOT FOUND\n")
+    print("\nNOT FOUND\n")
     return x
 
 def f(x):
