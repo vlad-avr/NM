bba00f1515a245d1ed5b7378a1f257ba05dea41a
vlad-avr <99638385+vlad-avr@users.noreply.github.com>
2023-11-15 14:50:43 +0200

    rewritten code (good approximation - bad roots)

--- Lab4/lab4.py
+++ Lab4/lab4.py
@@ -1,81 +1,74 @@
 import numpy as np
 import matplotlib.pyplot as plt
-from scipy.optimize import newton
 
-# Задане рівняння
+# Function to find the roots of the Chebyshev polynomial
+def chebyshev_roots(n):
+    return np.cos((2 * np.arange(1, n + 1) - 1) * np.pi / (2 * n))
+
+# Function to evaluate the given non-linear equation
 def equation(x):
     return x**3 - 4*x**2 - 4*x + 13 - np.sin(x)
 
-# Функція для побудови поліному Чебишова
-def chebyshev_poly(n):
-    x = np.cos((2 * np.arange(1, n + 1) - 1) * np.pi / (2 * n))
-    return x
-
-# Знаходимо нулі поліному Чебишова
-cheb_nodes = chebyshev_poly(10)
-
-# Виводимо нулі поліному Чебишова
-print("Чебишовські вузли:", cheb_nodes)
-
-# Побудова графіка рівняння та поліному Чебишова
-x_values = np.linspace(-2, 2, 1000)
-y_values_equation = equation(x_values)
-y_values_cheb = equation(cheb_nodes)
-
-plt.plot(x_values, y_values_equation, label='Рівняння')
-plt.scatter(cheb_nodes, y_values_cheb, color='red', label='Чебишовські вузли')
-plt.axhline(0, color='black', linewidth=0.5, linestyle='--')
-plt.legend()
-plt.title('Рівняння та його Чебишовські вузли')
-plt.xlabel('x')
-plt.ylabel('y')
-plt.show()
-
-# Пряма інтерполяція за допомогою поліному Ньютона
-def direct_interpolation(x, y, target_y):
-    n = len(x)
-    coefficients = np.zeros(n)
-    coefficients[0] = y[0]
+# Newton's interpolation polynomial coefficients
+def newton_coefficients(x_nodes, y_nodes):
+    n = len(x_nodes)
+    coefficients = y_nodes.copy()
 
     for j in range(1, n):
         for i in range(n - 1, j - 1, -1):
-            y[i] = (y[i] - y[i - 1]) / (x[i] - x[i - j])
+            coefficients[i] = (coefficients[i] - coefficients[i - 1]) / (x_nodes[i] - x_nodes[i - j])
 
-    result = coefficients[0]
+    return coefficients
 
-    for i in range(1, n):
-        result += coefficients[i] * (target_y - x[i - 1])
-        coefficients[i] = result
+# Newton's interpolation polynomial
+def newton_interpolation(x, x_nodes, coefficients):
+    n = len(x_nodes)
+    result = coefficients[-1]
+
+    for i in range(n - 2, -1, -1):
+        result = result * (x - x_nodes[i]) + coefficients[i]
 
     return result
 
-# Обернена інтерполяція за допомогою поліному Ньютона
-def inverse_interpolation(x, y, target_y):
-    n = len(x)
-    coefficients = np.zeros(n)
-    coefficients[0] = y[0]
+# Number of interpolation points
+n_points = 10
 
-    for j in range(1, n):
-        for i in range(n - 1, j - 1, -1):
-            y[i] = (y[i] - y[i - 1]) / (x[i] - x[i - j])
+# Chebyshev roots as interpolation points
+interpolation_points = chebyshev_roots(n_points)
 
-    result = coefficients[0]
+# Evaluate the function at interpolation points
+function_values = equation(interpolation_points)
 
-    for i in range(1, n):
-        result += coefficients[i] * (target_y - x[i - 1])
-        coefficients[i] = result
+# Compute Newton's interpolation coefficients
+coefficients = newton_coefficients(interpolation_points, function_values)
+print(coefficients)
 
-    # Знаходимо корінь поліному методом Ньютона
-    initial_guess = x[-1] + 0.1
-    root = newton(lambda z: direct_interpolation(x, y, z) - target_y, initial_guess)
+# Generate a fine grid of x values for plotting
+x_values = np.linspace(-1, 5, 1000)
 
-    return root
+equation_values = equation(x_values)
 
-# Знаходимо корінь методом прямої інтерполяції
-direct_root = direct_interpolation(cheb_nodes, y_values_cheb, 0)
+# Evaluate the interpolation polynomial at the fine grid
+interpolation_result = newton_interpolation(x_values, interpolation_points, coefficients)
 
-# Знаходимо корінь методом оберненої інтерполяції
-inverse_root = inverse_interpolation(cheb_nodes, y_values_cheb, 0)
+# Find roots of the interpolation polynomial (greatest root corresponds to the root of the original equation)
+roots = np.roots(np.flip(coefficients))
 
-print("Корінь за допомогою прямої інтерполяції:", direct_root)
-print("Корінь за допомогою оберненої інтерполяції:", inverse_root)
+print(roots)
+
+# Find the greatest root (real part) among the roots
+greatest_root = max([root.real for root in roots])
+
+# Plotting
+plt.plot(x_values, interpolation_result, label="Interpolation Polynomial")
+plt.plot(x_values, equation_values, label = "Function to approximate", linestyle = "--")
+plt.scatter(interpolation_points, function_values, color='red', label='Interpolation Points')
+plt.axhline(0, color='black', linestyle='--', linewidth=0.8)
+plt.xlabel('x')
+plt.ylabel('f(x)')
+plt.title('Newton\'s Interpolation for the Non-linear Equation')
+plt.legend()
+plt.grid(True)
+plt.show()
+
+print(f"The greatest root of the equation is: {greatest_root:.6f}")
