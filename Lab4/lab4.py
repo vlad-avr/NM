63c614576982b748773abff0ab229772c2030421
vlad-avr <99638385+vlad-avr@users.noreply.github.com>
2023-11-15 16:09:41 +0200

    its working

--- Lab4/lab4.py
+++ Lab4/lab4.py
@@ -1,78 +1,68 @@
 import numpy as np
 import matplotlib.pyplot as plt
-from scipy.optimize import minimize_scalar
+from scipy.interpolate import lagrange
+from scipy.optimize import newton
 
-# Function to find the roots of the Chebyshev polynomial
-def chebyshev_roots(n):
-    return np.cos((2 * np.arange(1, n + 1) - 1) * np.pi / (2 * n))
-
-# Function to evaluate the given non-linear equation
+# Define the equation for which you are looking for the root
 def equation(x):
-    #return x**3 - 4*x**2 - 4*x + 13 - np.sin(x)
-    return 3**x
+    return x**3 - 4*x**2 - 4*x + 13 - np.sin(x)
 
-# Newton's interpolation polynomial coefficients
-def newton_coefficients(x_nodes, y_nodes):
-    n = len(x_nodes)
-    coefficients = y_nodes.copy()
+# Function to find Chebyshev nodes
+def chebyshev_nodes(n):
+    nodes = [np.cos((2*k - 1) * np.pi / (2*n)) for k in range(1, n+1)]
+    return nodes
 
-    for j in range(1, n):
-        for i in range(n - 1, j - 1, -1):
-            coefficients[i] = (coefficients[i] - coefficients[i - 1]) / (x_nodes[i] - x_nodes[i - j])
 
-    return coefficients
+# Function to plot the function and the interpolated polynomial
+def plot_function_and_interpolation(equation, interp_poly, nodes, x_range, root_direct, root_inverse):
+    x_values = np.linspace(x_range[0], x_range[1], 1000)
+    y_function = equation(x_values)
+    y_interpolation = interp_poly(x_values)
 
-# Newton's interpolation polynomial
-def newton_interpolation(x, x_nodes, coefficients):
-    n = len(x_nodes)
-    result = coefficients[-1]
+    plt.plot(x_values, y_function, label="Original Function", linestyle="--")
+    plt.plot(nodes, equation(np.array(nodes)), 'o', label="Chebyshev Nodes")
+    plt.plot(x_values, y_interpolation, label="Interpolated Polynomial")
+    plt.axhline(0, x_range[0], x_range[1], linestyle="--", label="Y axis")
+    plt.plot(root_direct, equation(root_direct), 'o', label="Direct Root")
+    plt.plot(root_inverse, equation(root_inverse), 'o', label="Root Inverse")
+    plt.title("Function and Interpolated Polynomial")
+    plt.xlabel("x")
+    plt.ylabel("y")
+    plt.legend()
+    plt.grid(True)
+    plt.show()
 
-    for i in range(n - 2, -1, -1):
-        result = result * (x - x_nodes[i]) + coefficients[i]
+# Find the greatest root using inverse interpolation
+def find_greatest_root_inverse_interpolation():
+    n = 10
+    cheb_nodes = chebyshev_nodes(n)
+    f_values = equation(np.array(cheb_nodes))
+    interp_poly = lagrange(cheb_nodes, f_values)
 
-    return result
+    # Find the root using inverse interpolation
+    initial_guess = 4.0
+    root_inverse_interp = newton(interp_poly, initial_guess)
+    
+    return root_inverse_interp
 
-# Number of interpolation points
-n_points = 3
+# Find the greatest root using direct interpolation
+def find_greatest_root_direct_interpolation():
+    initial_guess = 5.0
+    root_direct_interp = newton(equation, initial_guess)
+    
+    return root_direct_interp
 
-# Chebyshev roots as interpolation points
-interpolation_points = chebyshev_roots(n_points)
-print(interpolation_points)
+# Main program
+greatest_root_inverse_interp = find_greatest_root_inverse_interpolation()
+greatest_root_direct_interp = find_greatest_root_direct_interpolation()
 
-# Evaluate the function at interpolation points
-function_values = equation(interpolation_points)
-print(function_values)
+print("Greatest root using inverse interpolation:", greatest_root_inverse_interp)
+print("Greatest root using direct interpolation:", greatest_root_direct_interp)
 
-# Compute Newton's interpolation coefficients
-coefficients = newton_coefficients(interpolation_points, function_values)
-print(coefficients)
-
-# Generate a fine grid of x values for plotting
-x_values = np.linspace(-1, 5, 1000)
-
-equation_values = equation(x_values)
-
-# Evaluate the interpolation polynomial at the fine grid
-interpolation_result = newton_interpolation(x_values, interpolation_points, coefficients)
-
-# Find roots of the interpolation polynomial (greatest root corresponds to the root of the original equation)
-roots = np.roots(np.flip(coefficients))
-
-print(roots)
-
-# Find the greatest root (real part) among the roots
-greatest_root = max([root.real for root in roots])
-
-# Plotting
-plt.plot(x_values, interpolation_result, label="Interpolation Polynomial")
-plt.plot(x_values, equation_values, label = "Function to approximate", linestyle = "--")
-plt.scatter(interpolation_points, function_values, color='red', label='Interpolation Points')
-plt.axhline(0, color='black', linestyle='--', linewidth=0.8)
-plt.xlabel('x')
-plt.ylabel('f(x)')
-plt.title('Newton\'s Interpolation for the Non-linear Equation')
-plt.legend()
-plt.grid(True)
-plt.show()
-
-print(f"The greatest root of the equation is: {greatest_root:.6f}")
+# Plot the function and the interpolated polynomial
+x_range = (-2, 5)
+n = 10
+cheb_nodes = chebyshev_nodes(n)
+f_values = equation(np.array(cheb_nodes))
+interp_poly = lagrange(cheb_nodes, f_values)
+plot_function_and_interpolation(equation, interp_poly, cheb_nodes, x_range, greatest_root_direct_interp, greatest_root_inverse_interp)
