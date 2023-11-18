ae59cdfe986085c7c7e7870c7d1bc453a53b6c78
vlad-avr <99638385+vlad-avr@users.noreply.github.com>
2023-11-18 15:26:43 +0200

    finished MS_lab3

--- MS_Lab3/Lab3.py
+++ MS_Lab3/Lab3.py
@@ -2,20 +2,53 @@ import sympy as sp
 import numpy as np
 
 
-def read(filename):
-    file = open(filename, 'r')
+def read_file(file_name):
+    file = open(file_name, 'r')
     lines = file.readlines()
-    data = []
+    input_data = []
     for line in lines:
         values = line.strip().split()
-        row_data = [float(value) for value in values]
-        data.append(row_data)
-    y = np.array(data).T
-    return y
+        row = []
+        for value in values:
+            row.append(float(value))
+        input_data.append(row)
+    return np.array(input_data).T
 
-def create_matrix_and_symbols():
+def get_derivative(y_vec, b_vec, b_values):
+    derivs = []
+    for y_i in y_vec:
+        for b_i in b_vec:
+            d = sp.diff(y_i, b_i)
+            d = d.subs(b_values)
+            derivs.append(d)
+
+    cols_n = len(b_vec)
+    der_matr = []
+    for i in range(0, len(derivs), cols_n):
+        der_matr.append(derivs[i:i + cols_n])   
+    return sp.Matrix(der_matr)
+
+
+def get_u_matr(a_matr, b_matr, u_matr, h):
+    b_arrayed = np.array(b_matr.tolist())
+    k1 = h * (np.dot(a_matr, u_matr) + b_arrayed)
+    k2 = h * (np.dot(a_matr, u_matr + k1 / 2) + b_arrayed)
+    k3 = h * (np.dot(a_matr, u_matr + k2 / 2) + b_arrayed)
+    k4 = h * (np.dot(a_matr, u_matr + k3) + b_arrayed)
+    return u_matr + (k1 + 2 * k2 + 2 * k3 + k4) / 6
+
+
+def get_y(a_matr, y_cur, h):
+    k1 = h * np.dot(a_matr, y_cur)
+    k2 = h * np.dot(a_matr, y_cur + k1 / 2)
+    k3 = h * np.dot(a_matr, y_cur + k2 / 2)
+    k4 = h * np.dot(a_matr, y_cur + k3)
+    return y_cur + (k1 + 2 * k2 + 2 * k3 + k4) / 6
+
+
+def init_matr():
     c1, c2, c3, c4, m1, m2, m3 = sp.symbols('c1 c2 c3 c4 m1 m2 m3')
-    matrix_a = [
+    matr = [
         [0, 1, 0, 0, 0, 0],
         [-(c2 + c1) / m1, 0, c2 / m1, 0, 0, 0],
         [0, 0, 0, 1, 0, 0],
@@ -23,146 +56,53 @@ def create_matrix_and_symbols():
         [0, 0, 0, 0, 0, 1],
         [0, 0, c3 / m3, 0, -(c4 + c3) / m3, 0]
     ]
-    return sp.Matrix(matrix_a)
+    return sp.Matrix(matr)
 
-
-def print_matrix(matrix, name='Matrix'):
-    print(f"\n{name}\n")
-    num_rows, num_cols = matrix.shape
-    max_widths = [max(len(str(matrix[i, j])) for i in range(num_rows)) for j in range(num_cols)]
-    for i in range(num_rows):
-        row_str = ""
-        for j in range(num_cols):
-            element = str(matrix[i, j])
-            padding = max_widths[j] - len(element)
-            row_str += element + ' ' * (padding + 1) + "\t\t"
-        print(row_str)
-
-
-def derivative_of_vector(y_vector, b_vector):
-    derivatives = [sp.diff(Yi, Bi) for Yi in y_vector for Bi in b_vector]
-    num_cols = len(b_vector)
-    derivative_matrix = [derivatives[i:i + num_cols] for i in range(0, len(derivatives), num_cols)]
-    return sp.Matrix(derivative_matrix)
-
-
-def to_y_map(y_matrix, i):
-    y1, y2, y3, y4, y5, y6 = sp.symbols('y1 y2 y3 y4 y5 y6')
-    return {
-        y1: y_matrix[i][0],
-        y2: y_matrix[i][1],
-        y3: y_matrix[i][2],
-        y4: y_matrix[i][3],
-        y5: y_matrix[i][4],
-        y6: y_matrix[i][5]
-    }
-
-
-def to_np_matrix(mtx):
-    return np.array(mtx.tolist())
-
-
-def to_y_map_simple(y_v):
-    y1, y2, y3, y4, y5, y6 = sp.symbols('y1 y2 y3 y4 y5 y6')
-    return {
-        y1: y_v[0],
-        y2: y_v[1],
-        y3: y_v[2],
-        y4: y_v[3],
-        y5: y_v[4],
-        y6: y_v[5]
-    }
-
-
-# 6 x 3
-def calculate_f(u_matrix, a_filled, b_filled, h=0.2):
-    return h * (np.dot(a_filled, u_matrix) + b_filled)
-
-
-def calculate_u(a_filled, b_matrix, u_curr):
-    b_filled = to_np_matrix(b_matrix)
-    k1 = calculate_f(u_curr, a_filled, b_filled)
-    k2 = calculate_f(u_curr + k1 / 2, a_filled, b_filled)
-    k3 = calculate_f(u_curr + k2 / 2, a_filled, b_filled)
-    k4 = calculate_f(u_curr + k3, a_filled, b_filled)
-    return u_curr + (k1 + 2 * k2 + 2 * k3 + k4) / 6
-
-
-def calculate_f_y(y_current, a_filled, h=0.2):
-    return h * (np.dot(a_filled, y_current))
-
-
-def calculate_y(a_filled, y_current):
-    k1 = calculate_f_y(y_current, a_filled)
-    k2 = calculate_f_y(y_current + k1 / 2, a_filled)
-    k3 = calculate_f_y(y_current + k2 / 2, a_filled)
-    k4 = calculate_f_y(y_current + k3, a_filled)
-    return y_current + (k1 + 2 * k2 + 2 * k3 + k4) / 6
-
-
-def y_one(y_matrix):
-    return y_matrix[0]
-
-
-def to_beta_vector(beta_init):
-    c1, c2, c3 = sp.symbols('c1 c2 c3')
-    return np.array([beta_init[c1], beta_init[c2], beta_init[c3]])
-
-
-def update_beta_init(beta_vector):
-    c1, c2, c3 = sp.symbols('c1 c2 c3')
-    return {
-        c1: beta_vector[0],
-        c2: beta_vector[1],
-        c3: beta_vector[2]
-    }
-
-
-# 0.14 0.3 0.2
-
-def calculate(y_matrix, params, beta, beta_init, eps):
-    a_matrix = create_matrix_and_symbols()
-    a_filled = a_matrix.subs(params)
-    print_matrix(a_filled, 'Matrix A')
-    beta_vector = to_beta_vector(beta_init)
+def approximate(y_matr, params, beta_symbols, beta_values, eps, h=0.2):
+    a_matrix = init_matr().subs(params)
+    # a_matrix = a_matrix.subs(params)
+    print(a_matrix)
+    beta_vector = np.array([beta_values[beta_symbols[0]], beta_values[beta_symbols[1]], beta_values[beta_symbols[2]]])
     while True:
-        a_extra_filled = to_np_matrix(a_filled.subs(beta_init))
-        u_matrix = np.zeros((6, 3))
-        to_inv_integral = np.zeros((3, 3))
-        to_mult_integral = np.zeros((1, 3))
-        distance_integral = 0
-        y_approx = y_one(y_matrix)
-        for i in range(len(y_matrix)):
-            ay_prod = a_filled * sp.Matrix(y_approx)
-            b_matrix = derivative_of_vector(ay_prod, beta)
+        a_complete = np.array((a_matrix.subs(beta_values)).tolist())
+        u_matr = np.zeros((6, 3))
+        delta_integral = 0
+        integral_part_inverse = np.zeros((3, 3))
+        integral_part_mult = np.zeros((1, 3))
+        y_approximation = y_matr[0]
+        for i in range(len(y_matr)):
+            b_derivative_matr = get_derivative(a_matrix * sp.Matrix(y_approximation), beta_symbols, beta_values)
 
-            prod = np.dot(u_matrix.T, u_matrix)
-            to_inv_integral = (to_inv_integral + prod).astype('float64')
+            integral_part_inverse = (integral_part_inverse + np.dot(u_matr.T, u_matr)).astype('float64')
 
-            mult_prod = np.dot(u_matrix.T, y_matrix[i] - y_approx)
-            to_mult_integral = (to_mult_integral + mult_prod).astype('float64')
+            integral_part_mult = (integral_part_mult + np.dot(u_matr.T, y_matr[i] - y_approximation)).astype('float64')
 
-            distance = np.dot(y_matrix[i] - y_approx, y_matrix[i] - y_approx)
-            distance_integral = distance_integral + distance
-
-            u_matrix = calculate_u(a_extra_filled, b_matrix, u_matrix)
-            y_approx = calculate_y(a_extra_filled, y_approx)
-        to_inv_integral = to_inv_integral * 0.2
-        to_mult_integral = to_mult_integral * 0.2
-        distance_integral = distance_integral * 0.2
-        vv = to_mult_integral.flatten()
-        delta_beta = np.dot(np.linalg.inv(to_inv_integral), vv)
+            delta_integral = delta_integral + np.dot(y_matr[i] - y_approximation, y_matr[i] - y_approximation)
+            
+            u_matr = get_u_matr(a_complete, b_derivative_matr, u_matr, h)
+            y_approximation = get_y(a_complete, y_approximation, h)
+            
+        integral_part_inverse = integral_part_inverse * h
+        integral_part_mult = integral_part_mult * h
+        delta_integral = delta_integral * h
+        
+        delta_beta = np.dot(np.linalg.inv(integral_part_inverse), integral_part_mult.flatten())
         beta_vector = beta_vector + delta_beta
-        beta_init = update_beta_init(beta_vector)
-        print("Beta", beta_vector)
-        print("Distance", distance_integral)
-        if distance_integral < eps:
-            return beta_init
+        
+        beta_values = {
+            beta_symbols[0]: beta_vector[0],
+            beta_symbols[1]: beta_vector[1],
+            beta_symbols[2]: beta_vector[2]
+        }
+        print("Current approximated values : ", beta_vector)
+        print("Delta : ", delta_integral)
+        if delta_integral < eps:
+            return beta_values
+        print("Delta is greater than ", eps, " -> next iteration")
         
         
-mtx_input = read('D:\\python\\NM\\MS_Lab3\\y1.txt')
+input = read_file('D:\\python\\NM\\MS_Lab3\\y1.txt')
 c1, c2, c3, c4, m1, m2, m3 = sp.symbols('c1 c2 c3 c4 m1 m2 m3')
-beta_init = {c1: 0.1, c2: 0.1, c3: 0.4}
-beta_init_real = {c1: 0.14, c2: 0.3, c3: 0.2}
-approximated = calculate(mtx_input, {m1: 12, m2: 28, m3: 18, c4: 0.12}, [c1, c2, c3], beta_init, 1e-6)
-print("Approximation:", approximated)
+to_approx = {c1: 0.1, m1: 11, m2: 23}
+result = approximate(input, {c2: 0.3, c3: 0.2, c4: 0.12, m3: 18}, [c1, m1, m2], to_approx, 1e-6)
+print("Approximation : ", result)
