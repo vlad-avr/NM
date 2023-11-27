b93cd6029802db862028bb2746878d182f5280ec
vlad-avr <99638385+vlad-avr@users.noreply.github.com>
2023-11-27 10:33:35 +0200

    fixed lab5

--- Lab5/lab5.py
+++ Lab5/lab5.py
@@ -1,46 +1,37 @@
 import numpy as np
-import math as math
+import math
 
 def f(x):
     return 1 / (2 + x)
 
 def f_second_der(x):
-    return 2/(2 + x)**3
-
-
+     return 2/(2 + x)**3
+ 
 def get_h(a, b, eps):
     M2 = max(abs(f_second_der(x)) for x in range(a, b + 1))
-    print(M2)
+    print("Maximum value of 2nd derivative : ", M2)
     h = np.sqrt((24*eps)/(M2 * (b-a)))
-    return h
+    approx = 0.0
+    while True:
+        if approx + 0.5 > h:
+            return approx
+        approx += 0.5
 
-def get_f_interval(a, n, h):
-    f_x = []
-    cur = a
-    next = a+h
-    for i in range(1, n):
-        f_x.append(f((cur + next)/2))
-        cur = next
-        next += h
-    return f_x
-        
+def midpoint_rule(a, b, n):
+    h = (b - a) / n
+    result = 0
+    for i in range(n):
+        xi = a + (i + 0.5) * h
+        result += f(xi)
+    result *= h
+    return result
 
-def calculate_midpoint(f_x, h):
-    res = 0
-    for i in f_x:
-        res += i
-    res *= h
-    return res       
-        
 
 a = 1
 b = 5
-eps = 0.05
-
-# Result using regular integral computation : 0.8473
-#h = math.floor(get_h(a, b, eps))
-h = 0.1
-n = (int)((b-a)/h)
-f_x = get_f_interval(a, n, h)
-result = calculate_midpoint(f_x, h)
-print("The result of the integral is: ", result)
+h = get_h(a, b, 0.05)
+print("h = ", h)
+n = math.floor((b-a)/h)
+print("n = ", n)
+result = midpoint_rule(a, b, n)
+print(f"The result of the definite integral is: {result}")
